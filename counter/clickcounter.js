f16d509eb78543e3aff6dc5b98e37707d04d8c20
Sravanthi <sravanthigurgubelli@gmail.com>
2024-10-11 23:15:23 +0530

    Alert&Window

--- counter/clickcounter.js
+++ counter/clickcounter.js
@@ -4,11 +4,15 @@ function coun() {
    counter++
   if (counter > 5) {
     document.getElementById("dolly").disabled=true
-
   }
  else {
     document.getElementById("counter").innerText = "you clicked" + counter + "times"
-  }
- 
-  
+  } 
+}
+
+function alertt() {
+  alert("you are not allowed")
+}
+function neww() {
+  window.print()
 }
